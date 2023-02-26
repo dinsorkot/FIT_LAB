85827b59f6285362739542c7e8cb9e32cd6b5f1d
NapatBourban <napatone123@gmail.com>
2023-02-26 21:55:20 +0700

    finish

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      ohm,japansudtae
+      ohm,japansudtae.Nueng
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
