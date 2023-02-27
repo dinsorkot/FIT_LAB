3ef18aab91d8a870321801cb0e2830a23fc81ef8
Sunz098 <bazada147@gmail.com>
2023-02-27 22:25:30 +0700

    xxx

--- src/component/profile.vue
+++ src/component/profile.vue
@@ -1,5 +1,48 @@
 <template>
+  <div class="container d-flex justify-content-center" style="margin: 20px">
     <div>
-        profile
+      <div class="card" style="width: 30rem; height: 15rem">
+        <div
+          class="pencilicon"
+          style="
+            display: flex;
+            justify-content: right;
+            margin-right: 5px;
+            margin-top: 5px;
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-pencil"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
+            />
+          </svg>
+        </div>
+        <div class="card-body">
+          <div class="card" style="height: 150px; width: 150px; display:flex;justify-content: center; justify-items: center;" >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="150"
+              height="100"
+              fill="currentColor"
+              class="bi bi-person"
+              viewBox="0 0 16 16"
+              
+            >
+              <path
+                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
+              />
+            </svg>
+          </div>
+          
+        </div>
+      </div>
     </div>
+  </div>
 </template>
