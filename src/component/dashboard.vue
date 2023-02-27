d4a646890d9e4677238b58d5ab8e88fa479022bd
dinsorkot <mju372ohm@gmail.com>
2023-02-28 02:17:45 +0700

    แก้ของแปนนแล้ว

--- src/component/dashboard.vue
+++ src/component/dashboard.vue
@@ -1,133 +1,100 @@
 <template>
-  <div class="text-white m-5">
-    <h3>Overview Monitor</h3>
-  </div>
-  <div class="container text-white">
-    <div class="row">
-      <div class="col">
-        <div
-          class="row w-100 color"
-          style="width: 20rem; height: 15rem; border-radius: 25px"
-        >
-          <div class="col">
-            <div
-              class="bg-white w-60 h-55 m-4"
-              style="border-radius: 25px; height: 100px"
-            >
-              <div class="text-dark d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100"
-                  height="100"
-                  fill="currentColor"
-                  class="bi bi-1-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312V4.002Z"
-                  />
-                </svg>
+  <div class="container text-white mt-5">
+    <div class="py-3">
+      <h3>Overview Monitor</h3>
+    </div>
+    <div class="container ">
+      <div class="row">
+        <div class="col">
+          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
+            <div class="col d-flex justify-content-center align-items-center">
+              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+                <div class="text-dark d-flex justify-content-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                    class="bi bi-1-circle-fill" viewBox="0 0 16 16">
+                    <path
+                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312V4.002Z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col mt-4">
-            <div class="row">Calories</div>
-            <div class="row">2456</div>
-            <div class="row">DAILY AVERAGE</div>
+            <div class="col mt-4">
+              <div class="row">Calories</div>
+              <div class="row">2456</div>
+              <div class="row">DAILY AVERAGE</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col">
-        <div
-          class="row w-100 color"
-          style="width: 20rem; height: 15rem; border-radius: 25px"
-        >
-          <div class="col">
-            <div
-              class="bg-white w-60 h-55 m-4"
-              style="border-radius: 25px; height: 100px"
-            >
-              <div class="text-dark d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100"
-                  height="100"
-                  fill="currentColor"
-                  class="bi bi-2-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z"
-                  />
-                </svg>
+        <div class="col">
+          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
+            <div class="col">
+              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+                <div class="text-dark d-flex justify-content-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                    class="bi bi-2-circle-fill" viewBox="0 0 16 16">
+                    <path
+                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col">
-            <div class="row">Workouts</div>
-            <div class="row">1 h 23 min</div>
-            <div class="row">ACTIVE</div>
+            <div class="col">
+              <div class="row">Workouts</div>
+              <div class="row">1 h 23 min</div>
+              <div class="row">ACTIVE</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col">
-        <div
-          class="row w-100 color"
-          style="width: 20rem; height: 15rem; border-radius: 25px"
-        >
-          <div class="col">
-            <div
-              class="bg-white w-60 h-55 m-4"
-              style="border-radius: 25px; height: 100px"
-            >
-              <div class="text-dark d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100"
-                  height="100"
-                  fill="currentColor"
-                  class="bi bi-3-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z"
-                  />
-                </svg>
+        <div class="col">
+          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
+            <div class="col">
+              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+                <div class="text-dark d-flex justify-content-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                    class="bi bi-3-circle-fill" viewBox="0 0 16 16">
+                    <path
+                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col">
-            <div class="row">Sleep Analysis</div>
-            <div class="row">1 h 23 min</div>
-            <div class="row">IN BAD AVERAGE</div>
+            <div class="col">
+              <div class="row">Sleep Analysis</div>
+              <div class="row">1 h 23 min</div>
+              <div class="row">IN BAD AVERAGE</div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="d-flex text-white" style="height: 50vh">
-    <div class="color m-5" style="width: 60vh; border-radius: 25px">
-      <div class="d-flex">
-        <div class="row">
-          <div class="col w-100 h-100">
-            <div
-              class="bg-white mt-5"
-              style="width: 150px; height: 200px; margin-left: 20%; border-radius: 100px"
-            ></div>
-          </div>
-          <div class="col m-5" style="width: 50vh; height: 100%">
-            <div class="row"><h3>Calories</h3></div>
-            <div class="row">...% ...%</div>
-            <div class="row"><h3>Workouts</h3></div>
-            <div class="row">...% ...%</div>
-            <div class="row"><h4>Sleep Analysis</h4></div>
-            <div class="row">...% ...%</div>
+    <div class="d-flex text-white" style="height: 50vh">
+      <div class="color m-5" style="width: 60vh; border-radius: 25px">
+        <div class="d-flex">
+          <div class="row">
+            <div class="col w-100 h-100">
+              <div class="bg-white mt-5" style="width: 150px; height: 200px; margin-left: 20%; border-radius: 100px">
+              </div>
+            </div>
+            <div class="col m-5" style="width: 50vh; height: 100%">
+              <div class="row">
+                <h3>Calories</h3>
+              </div>
+              <div class="row">...% ...%</div>
+              <div class="row">
+                <h3>Workouts</h3>
+              </div>
+              <div class="row">...% ...%</div>
+              <div class="row">
+                <h4>Sleep Analysis</h4>
+              </div>
+              <div class="row">...% ...%</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="m-5 color" style="width: 92vh; border-radius: 25px">
-      <h1 class="d-flex justify-content-center d-flex align-items-center mt-5">GRAPH</h1>
+      <div class="m-5 color" style="width: 92vh; border-radius: 25px">
+        <h1 class="d-flex justify-content-center d-flex align-items-center mt-5">GRAPH</h1>
+      </div>
     </div>
   </div>
 </template>
--- src/component/profile.vue
+++ src/component/profile.vue
@@ -1,89 +1,51 @@
 <template>
-  <div class="container d-flex justify-content-center" style="margin: 20px">
-    <div>
-      <div class="card" style="width: 70rem; height: 15rem; background-color: #1e1e1e">
-        <div
-          class="pencilicon"
-          style="
-            display: flex;
-            justify-content: right;
-            margin-right: 5px;
-            margin-top: 5px;
-          "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-pencil"
-            viewBox="0 0 16 16"
-          >
+  <div class="container">
+    <div class="container mt-5 btnclick">
+      <div class="d-flex position-relative  align-items-center h-100 ms-5">
+        <div class="card d-flex align-items-center justify-content-center"
+          style="background-color: #121212;width:150px; height:150px; border-radius: 25px;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor"
+            class="bi bi-person text-light" viewBox="0 0 16 16">
             <path
-              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
-            />
+              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
           </svg>
         </div>
-        <div class="card-body d-flex">
-          <span
-            class="card"
-            style="
-              height: 150px;
-              width: 150px;
-              display: flex;
-              justify-content: center;
-              justify-items: center;
-            "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="150"
-              height="100"
-              fill="currentColor"
-              class="bi bi-person"
-              viewBox="0 0 16 16"
-            >
+        <div class=" ms-5">
+          <h2 class="text-light"> USERNAME ( age ) </h2>
+          <h6 style="color:#898989;">sex</h6>
+          <h6 style="color:#898989;">weight , height</h6>
+        </div>
+        <div class="position-absolute top-0 end-0">
+          <a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-pencil text-light me-3 mt-3" viewBox="0 0 16 16">
               <path
-                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
-              />
-            </svg>
-          </span>
-          <span class="Gropprofile" style="margin-left: 10px; color: white">
-            <div class="Usernametext" style="font-size: 20px">Username (age)</div>
-            <div class="Sextext" style="font-size: 16px">sex</div>
-            <div class="Weighttext" style="font-size: 16px">weight</div>
-            <div class="Heighttext" style="font-size: 16px">height</div>
-          </span>
+                d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+            </svg></a>
         </div>
       </div>
-      <div>
-        <div class="row">
-          <div class="col">
-            <div
-              class="container border"
-              style="width: 35rem; height: 25rem; margin-top: 15px"
-            >
-              <div class="d-flex justify-content-center" style="color: white">
-                body type
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="container border p-5 my-3 border">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="currentColor"
-                  class="bi bi-person"
-                  viewBox="0 0 16 16"
-                >
+    </div>
+    <div class="container mt-4">
+      <div class="row gap-4">
+        <div class="col-4 d-flex justify-content-center align-items-center text-light"
+          style="height: 30rem; background-color:#1E1E1E;border-radius: 25px;">
+          <div> body type</div>
+        </div>
+        <div class="col h-100 d-flex justify-content-center  text-light">
+          <div class="container btnclick">
+            <div class="d-flex align-items-center h-100 ms-5">
+              <div class="card d-flex align-items-center justify-content-center"
+                style="background-color: #121212;width:80px; height:80px; border-radius: 25px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                  class="bi bi-person text-light" viewBox="0 0 16 16">
                   <path
-                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
-                  />
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                 </svg>
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              </div>
+              <div class=" ms-5">
+                <form class="form-floating">
+                  <input type="text" class="form-control rounded-pill border-0 input_EPU" id="post" placeholder="post">
+                  <label class="ps-4" for="post">what happend ?</label>
+                </form>
               </div>
             </div>
           </div>
@@ -92,3 +54,30 @@
     </div>
   </div>
 </template>
+<style scoped>
+.btnclick {
+  background-color: #1E1E1E;
+  height: 200px;
+  border-radius: 25px;
+  transition: 0.5s;
+}
+
+input {
+  background-color: #272727;
+  color: white;
+  height: 50px;
+  width: 35rem;
+  text-align: center;
+}
+
+input:focus {
+  background-color: #272727;
+  color: white;
+  text-align: center;
+}
+
+input::placeholder {
+  color: white;
+  text-align: center;
+}
+</style>
