8ba1361b83d50b5d5658f9599bfdd2d5229e8845
Sunz098 <bazada147@gmail.com>
2023-02-27 11:07:07 +0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBn5QN321IqjwuxpzJydeFSPocTPUj0KKY",
+  authDomain: "fitlab-7a350.firebaseapp.com",
+  projectId: "fitlab-7a350",
+  storageBucket: "fitlab-7a350.appspot.com",
+  messagingSenderId: "185196850905",
+  appId: "1:185196850905:web:65be234f887e5eac2e58c8",
+  measurementId: "G-S2GMKK437E"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+//firebase
+import './firebase.js';
 
 import './assets/main.css'
 
