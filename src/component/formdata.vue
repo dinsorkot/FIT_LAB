c9befa8f9e31a17270c577d50e99c7798c99f69a
Sunz098 <bazada147@gmail.com>
2023-02-28 11:08:12 +0700

    หน้าform

--- src/component/formdata.vue
+++ src/component/formdata.vue
@@ -1,5 +1,18 @@
+<script setup></script>
+
 <template>
-    <div>
-        form
+  <div class="container text-white">
+    <div class="py-3">
+      <h3>Calculate BMI</h3>
     </div>
+    <div class="container">
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+      <div class="container">loop</div>
+    </div>
+  </div>
 </template>
