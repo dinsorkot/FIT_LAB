ed5087ec8b4a957d0c36efb33feb8c0d37db719c
NapatBourban <napatone123@gmail.com>
2023-02-28 11:07:38 +0700

    dashboard

--- src/component/dashboard.vue
+++ src/component/dashboard.vue
@@ -1,64 +1,159 @@
 <template>
   <div class="container text-white mt-5">
     <div class="py-3">
-      <h3>Overview Monitor</h3>
+      <h2>Overview Monitor</h2>
     </div>
-    <div class="container ">
+    <div class="container">
       <div class="row">
         <div class="col">
-          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
+          <div
+            class="row w-100 color"
+            style="width: 20rem; height: 15rem; border-radius: 25px"
+          >
             <div class="col d-flex justify-content-center align-items-center">
-              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+              <div
+                class="colorbackicon w-60 h-55 m-4"
+                style="border-radius: 25px; height: 100px"
+              >
                 <div class="text-dark d-flex justify-content-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
-                    class="bi bi-1-circle-fill" viewBox="0 0 16 16">
-                    <path
-                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312V4.002Z" />
+                  <svg
+                    fill="#ffffff"
+                    width="100px"
+                    height="100px"
+                    viewBox="0 -3.84 122.88 122.88"
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    style="enable-background: new 0 0 122.88 115.21"
+                    xml:space="preserve"
+                    stroke="#ffffff"
+                    transform="matrix(1, 0, 0, 1, 0, 0)"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <g>
+                        <path
+                          d="M29.03,100.46l20.79-25.21l9.51,12.13L41,110.69C33.98,119.61,20.99,110.21,29.03,100.46L29.03,100.46z M53.31,43.05 c1.98-6.46,1.07-11.98-6.37-20.18L28.76,1c-2.58-3.03-8.66,1.42-6.12,5.09L37.18,24c2.75,3.34-2.36,7.76-5.2,4.32L16.94,9.8 c-2.8-3.21-8.59,1.03-5.66,4.7c4.24,5.1,10.8,13.43,15.04,18.53c2.94,2.99-1.53,7.42-4.43,3.69L6.96,18.32 c-2.19-2.38-5.77-0.9-6.72,1.88c-1.02,2.97,1.49,5.14,3.2,7.34L20.1,49.06c5.17,5.99,10.95,9.54,17.67,7.53 c1.03-0.31,2.29-0.94,3.64-1.77l44.76,57.78c2.41,3.11,7.06,3.44,10.08,0.93l0.69-0.57c3.4-2.83,3.95-8,1.04-11.34L50.58,47.16 C51.96,45.62,52.97,44.16,53.31,43.05L53.31,43.05z M65.98,55.65l7.37-8.94C63.87,23.21,99-8.11,116.03,6.29 C136.72,23.8,105.97,66,84.36,55.57l-8.73,11.09L65.98,55.65L65.98,55.65z"
+                        ></path>
+                      </g>
+                    </g>
                   </svg>
                 </div>
               </div>
             </div>
-            <div class="col mt-4">
-              <div class="row">Calories</div>
-              <div class="row">2456</div>
-              <div class="row">DAILY AVERAGE</div>
+            <div class="col" style="margin-top: 50px">
+              <div ><h3>Calories</h3></div>
+              <div ><h1>2456</h1></div>
+              <div class="colorfontmini">DAILY AVERAGE</div>
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
-            <div class="col">
-              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+          <div
+            class="row w-100 color"
+            style="width: 20rem; height: 15rem; border-radius: 25px"
+          >
+            <div class="col d-flex justify-content-center align-items-center">
+              <div
+                class="colorbackicon w-60 h-55 m-4"
+                style="border-radius: 25px; height: 100px"
+              >
                 <div class="text-dark d-flex justify-content-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
-                    class="bi bi-2-circle-fill" viewBox="0 0 16 16">
-                    <path
-                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z" />
+                  <svg
+                    fill="#f6f4f4"
+                    height="99px"
+                    width="99px"
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 512.001 512.001"
+                    xml:space="preserve"
+                    transform="matrix(-1, 0, 0, 1, 0, 0)"
+                    stroke="#f6f4f4"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        d="M103.706,399.437C46.523,399.437,0,352.915,0,295.731s46.523-103.705,103.706-103.705s103.706,46.521,103.706,103.705 S160.89,399.437,103.706,399.437z M103.706,214.035c-45.048,0-81.697,36.648-81.697,81.696c0,45.049,36.649,81.697,81.697,81.697 s81.697-36.649,81.697-81.697C185.402,250.683,148.754,214.035,103.706,214.035z"
+                      ></path>
+                      <path
+                        d="M408.294,399.437c-57.184,0-103.706-46.523-103.706-103.706s46.523-103.705,103.706-103.705 c6.442,0,12.892,0.595,19.171,1.77c5.974,1.117,9.911,6.865,8.794,12.839c-1.117,5.975-6.859,9.904-12.84,8.795 c-4.948-0.925-10.036-1.395-15.125-1.395c-45.048,0-81.697,36.649-81.697,81.696c0,45.049,36.649,81.697,81.697,81.697 s81.697-36.649,81.697-81.697c0-18.99-6.66-37.487-18.752-52.083c-3.877-4.68-3.227-11.617,1.454-15.494 c4.679-3.878,11.617-3.229,15.495,1.453c15.356,18.536,23.813,42.02,23.813,66.125C512,352.915,465.477,399.437,408.294,399.437z"
+                      ></path>
+                      <path
+                        d="M456.949,227.821c-1.975,0-3.974-0.53-5.777-1.645c-0.604-0.374-1.215-0.741-1.83-1.098 c-5.254-3.056-7.035-9.792-3.978-15.045c3.056-5.254,9.791-7.038,15.045-3.978c0.786,0.457,1.565,0.924,2.337,1.401 c5.169,3.196,6.769,9.978,3.573,15.148C464.237,225.968,460.635,227.821,456.949,227.821z"
+                      ></path>
+                      <path
+                        d="M103.706,306.739c-1.201,0-2.421-0.198-3.619-0.615c-5.74-1.998-8.773-8.272-6.774-14.011l54.851-157.537h-29.821 c-6.999,0-12.694,5.695-12.694,12.694c0,6.077-4.927,11.004-11.004,11.004c-6.078,0-11.004-4.928-11.004-11.004 c0-19.136,15.567-34.703,34.703-34.703h45.305c3.563,0,6.905,1.724,8.97,4.63c2.063,2.905,2.594,6.628,1.422,9.993l-59.943,172.16 C112.518,303.891,108.259,306.739,103.706,306.739z"
+                      ></path>
+                      <path
+                        d="M408.294,306.735H280.048c-3.031,0-5.927-1.25-8.007-3.455L143.255,166.692c-3.013-3.196-3.837-7.877-2.097-11.909 c1.741-4.033,5.711-6.645,10.104-6.645h167.121c3.702,0,7.156,1.861,9.192,4.954l89.91,136.588 c2.225,3.379,2.415,7.709,0.495,11.271C416.061,304.514,412.34,306.735,408.294,306.735z M284.797,284.726h103.078l-75.423-114.579 h-135.69L284.797,284.726z"
+                      ></path>
+                      <path
+                        d="M280.054,306.739c-0.985,0-1.985-0.133-2.98-0.413c-5.851-1.642-9.264-7.717-7.622-13.569L307.751,156.3l9.029-35.445 c1.501-5.891,7.493-9.446,13.38-7.947c5.89,1.5,9.447,7.491,7.947,13.38l-9.061,35.572c-0.022,0.086-0.045,0.172-0.069,0.256 l-38.335,136.588C289.28,303.562,284.862,306.739,280.054,306.739z"
+                      ></path>
+                      <path
+                        d="M358.287,134.576h-61.686c-6.078,0-11.004-4.928-11.004-11.004s4.927-11.004,11.004-11.004h61.686 c6.078,0,11.004,4.928,11.004,11.004S364.365,134.576,358.287,134.576z"
+                      ></path>
+                    </g>
                   </svg>
                 </div>
               </div>
             </div>
-            <div class="col">
-              <div class="row">Workouts</div>
-              <div class="row">1 h 23 min</div>
-              <div class="row">ACTIVE</div>
+            <div class="col" style="margin-top: 75px">
+              <div >Workouts</div>
+              <div >1 h 23 min</div>
+              <div class="colorfontmini">ACTIVE</div>
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="row w-100 color" style="width: 20rem; height: 15rem; border-radius: 25px">
-            <div class="col">
-              <div class="bg-white w-60 h-55 m-4" style="border-radius: 25px; height: 100px">
+          <div
+            class="row w-100 color"
+            style="width: 20rem; height: 15rem; border-radius: 25px"
+          >
+            <div class="col d-flex justify-content-center align-items-center">
+              <div
+                class="colorbackicon w-60 h-55 m-4"
+                style="border-radius: 25px; height: 100px"
+              >
                 <div class="text-dark d-flex justify-content-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
-                    class="bi bi-3-circle-fill" viewBox="0 0 16 16">
-                    <path
-                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z" />
+                  <svg
+                    width="100px"
+                    height="100px"
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#ffffff"
+                    stroke="#ffffff"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        fill="#ffffff"
+                        d="M240.448 240.448a384 384 0 1 0 559.424 525.696 448 448 0 0 1-542.016-542.08 390.592 390.592 0 0 0-17.408 16.384zm181.056 362.048a384 384 0 0 0 525.632 16.384A448 448 0 1 1 405.056 76.8a384 384 0 0 0 16.448 525.696z"
+                      ></path>
+                    </g>
                   </svg>
                 </div>
               </div>
             </div>
-            <div class="col">
+            <div class="col" style="margin-top: 75px">
               <div class="row">Sleep Analysis</div>
               <div class="row">1 h 23 min</div>
               <div class="row">IN BAD AVERAGE</div>
@@ -67,33 +162,50 @@
         </div>
       </div>
     </div>
-    <div class="d-flex text-white" style="height: 50vh">
-      <div class="color m-5" style="width: 60vh; border-radius: 25px">
+
+    <div class="contianer d-flex text-white" style="height: 50vh">
+      <div
+        class="color mt-5"
+        style="width: 60vh; border-radius: 25px; margin-bottom: 50px"
+      >
         <div class="d-flex">
           <div class="row">
             <div class="col w-100 h-100">
-              <div class="bg-white mt-5" style="width: 150px; height: 200px; margin-left: 20%; border-radius: 100px">
-              </div>
+              <div
+                class=" my-5  border border-white border-2"
+                style="height: 200px; margin-left: 20%;border-radius: 100%; "
+              ></div>
             </div>
-            <div class="col m-5" style="width: 50vh; height: 100%">
+            <div class="col m-5" style="width: 65vh; height: 100%">
               <div class="row">
                 <h3>Calories</h3>
               </div>
-              <div class="row">...% ...%</div>
+              <div class="row">
+                <div class="col">...%</div>
+                <div class="col">....%</div>
+              </div>
               <div class="row">
                 <h3>Workouts</h3>
               </div>
-              <div class="row">...% ...%</div>
+              <div class="row">
+                <div class="col">...%</div>
+                <div class="col">....%</div>
+              </div>
               <div class="row">
                 <h4>Sleep Analysis</h4>
               </div>
-              <div class="row">...% ...%</div>
+              <div class="row">
+                <div class="col">...%</div>
+                <div class="col">....%</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div class="m-5 color" style="width: 92vh; border-radius: 25px">
-        <h1 class="d-flex justify-content-center d-flex align-items-center mt-5">GRAPH</h1>
+        <h1 class="d-flex justify-content-center d-flex align-items-center mt-5">
+          GRAPH
+        </h1>
       </div>
     </div>
   </div>
@@ -102,5 +214,16 @@
 <style>
 .color {
   background-color: #1e1e1e;
+}
+
+.colorbackicon{
+  background-color: #121212
+;
+}
+.colorfontmini{
+ color: #6B6B6B
+;
+font-size:small;
+
 }
 </style>
