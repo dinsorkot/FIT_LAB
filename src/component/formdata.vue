7b0571118c090908218c03b0eb242cad2711ff90
Sunz098 <bazada147@gmail.com>
2023-02-28 14:21:36 +0700

    fprm

--- src/component/formdata.vue
+++ src/component/formdata.vue
@@ -6,13 +6,47 @@
       <h3>Calculate BMI</h3>
     </div>
     <div class="container">
-        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-      <div class="container">loop</div>
+      <div class="form-group">
+        <label for="ControlSelect">Please select the number of exercises </label>
+        <select class="form-control" id="ControlSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+        </select>
+      </div>
+      <div class="summit mt-2">
+        <button type="button" class="btn btn-secondary">SUMMIT</button>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-4 d-flex justify-content-center">
+            <div
+              class="card d-flex justify-content-center"
+              style="width: 20rem; height: 13rem"
+            >
+              <div class="d-flex justify-content-center">
+                <div>
+                  <div><input type="date" /></div>
+                  <div>
+                    <select name="" id="">
+                      <option selected>sdasdasdasdas</option>
+                      <option value="">1</option>
+                      <option value="">1</option>
+                      <option value="">1</option>
+                    </select>
+                  </div>
+                  <div><input type="number" name="" id="" placeholder="Hour" /></div>
+                  <div><input type="number" name="" id="" placeholder="minute" /></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
