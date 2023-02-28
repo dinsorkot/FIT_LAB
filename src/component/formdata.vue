d3bae185f95a34c1354e4d0dda32af1ef75e1514
Sunz098 <bazada147@gmail.com>
2023-02-28 15:50:34 +0700

    loopopen

--- src/component/formdata.vue
+++ src/component/formdata.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+  const lo  = ref(); 
+</script>
 
 <template>
   <div class="container text-white">
@@ -23,15 +26,15 @@
 
       <div class="container">
         <div class="row">
-          <div class="col-4 d-flex justify-content-center">
+          <div class="col-4 mt-3 d-flex justify-content-center" v-for="n in 3">
             <div
               class="card d-flex justify-content-center"
-              style="width: 20rem; height: 13rem"
+              style="width: 20rem; height: 13rem; background-color: #272727"
             >
               <div class="d-flex justify-content-center">
                 <div>
-                  <div><input type="date" /></div>
-                  <div>
+                  <div class="pb-2"><input type="date" /></div>
+                  <div class="pb-2">
                     <select name="" id="">
                       <option selected>sdasdasdasdas</option>
                       <option value="">1</option>
@@ -39,8 +42,11 @@
                       <option value="">1</option>
                     </select>
                   </div>
-                  <div><input type="number" name="" id="" placeholder="Hour" /></div>
-                  <div><input type="number" name="" id="" placeholder="minute" /></div>
+                  <div class="pb-2">
+                    <input type="number" placeholder="minute" maxlenght="2" />
+                  </div>
+                  <div><input type="number" placeholder="minute" maxlength="2" /></div>
+                  <div class="d-flex mt-2 justify-content-center"><button type="button" class="btn btn-success">ยืนยัน</button></div>
                 </div>
               </div>
             </div>
